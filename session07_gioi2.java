87596b39f6c0bb2239651261437a2259574dc1b6
NguyenSang94 <nguyendoansangz@gmail.com>
2025-12-17 11:08:44 +0700

    Add files via upload

---
+++ session07_gioi2.java
@@ -0,0 +1,13 @@
+package session07_Gioi2;
+
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    public void printInfo(){
+        System.out.println("title:"+title);
+        System.out.println("author:"+author);
+        System.out.println("price:"+price);
+    }
+}
